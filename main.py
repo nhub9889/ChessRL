59f0e233edb70088c3cfedcb5dfbf7f84b39325d
nhub9889 <nhub9889@gmail.com>
2025-09-19 18:42:31 +0700

    decrease batchsize

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ parser.add_argument('--pgn_path', type = str, default= "data")
 parser.add_argument('--model_path', type = str, default= "checkpoints")
 parser.add_argument('--device', type = str, default= 'cuda')
 parser.add_argument('--output', type = str, default = 'model')
+parser.add_argument('--batch_size', type = int, default= 128)
+parser.add_argument('--supervised_batch_size', type = int, default= 64)
+parser.add_argument('--num_workers', type= int, default= 4)
 def main():
     args = parser.parse_args()
     device = torch.device(args.device)
@@ -19,7 +22,8 @@ def main():
         path = args.pgn_path
         output_path = args.output
         visualizer = Visualizer()
-        pipeline = TrainingPipeline(model, visualizer= visualizer)
+        pipeline = TrainingPipeline(model, visualizer= visualizer, batch_size= args.batch_size,
+                                    supervised_batch_size= args.supervised_batch_size, num_workers= args.num_workers)
         pipeline.supervised_train(path)
 
         pipeline.train()
